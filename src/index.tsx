fae10d9ed7d283630188f308f4ad388cc238d57c
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-12 18:57:44 -0300

    Importar dependencia React-query em index.tsx, chamar o construtor do objeto, envolver o componente App.tsx no seu provider e passar o objeto queryClient como prop

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 //import './index.css';
 import App from './App';
 
 
 
+const client = new QueryClient();
+
 ReactDOM.render(
-    <App />,
+  <QueryClientProvider client={client}>  
+		<App />
+	</QueryClientProvider>,
   document.getElementById('root')
 );
 
