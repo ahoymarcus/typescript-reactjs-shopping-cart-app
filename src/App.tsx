2daa1a98fe8c442402e8811e8ddb4cdac27a045c
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-12 19:45:08 -0300

    Criar funcao de fetch. Criar type e usar como retorno para a funcao de fetch. Usar useQuery para controlar funcao de fetch. Logar os dados no console.

--- src/App.tsx
+++ src/App.tsx
@@ -15,10 +15,27 @@ import Badge from '@material-ui/core/Badge';
 // Styles
 import { Wrapper } from './App.styles';
 
+// types
+export type CartItemType = {
+	id: number;
+	category: string;
+	description: string;
+	image: string;
+	price: number;
+	title: string;
+	amount: number; 
+};
 
+
+const getProducts = async(): Promise<CartItemType[]> => {
+	return await (await fetch('https://fakestoreapi.com/products')).json();
+};
 
 const App = () => {
-  
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
+	
+	console.log(data);
+	
 	
 	return (
     <div className="App">
