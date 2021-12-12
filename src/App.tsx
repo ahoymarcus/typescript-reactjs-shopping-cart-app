c2041cded208097376e38761bf29d9c639ca4d65
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-12 19:17:35 -0300

    Importar useState, useQuery e componentes de Material-ui em App.tsx. Criar modulo App.styles.ts e importar no componente App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 // https://www.youtube.com/watch?v=sfmL6bGbiN8
-//  0hs 2'  14''
+//  0hs 11'  10''
+import { useState } from 'react';
+import { useQuery } from 'react-query';
 
 //import './App.css';
+
+// Components  
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+
+// Styles
+import { Wrapper } from './App.styles';
 
 
 
@@ -16,3 +28,5 @@ const App = () => {
 }
 
 export default App;
+
+
