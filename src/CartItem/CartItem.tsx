79cd8f7bf163c038b4e76f040238038f8ade87ec
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-13 19:53:38 -0300

    Inserir estilo para os itens no carrinho de compra

--- src/CartItem/CartItem.tsx
+++ src/CartItem/CartItem.tsx
@@ -7,7 +7,25 @@ import { CartItemType } from '../App';
 
 // Styles
 const Wrapper = styled.div`
-
+	display: flex;
+	justify-content: space-between;
+	font-family: Arial, Helvetica, sans-serif;
+	border-bottom: 1px solid lightblue;
+	
+	div {
+		flex: 1;
+	}
+	
+	.information, .buttons {
+		display: flex;
+		justify-content: space-between;
+	}
+	
+	img {
+		max-width: 80px;
+		object-fit: cover;
+		margin-left: 40px;
+	}
 `;
 
 
