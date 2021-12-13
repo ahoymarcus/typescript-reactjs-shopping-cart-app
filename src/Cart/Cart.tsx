c76754d84cff589c3d223b310b28ffe1707f911c
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-13 20:20:55 -0300

    Refatorar funcao para renderizar o total agregado dos items no carrinho de compras

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -22,7 +22,15 @@ type Props = {
 
 
 const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
-	const calculateTotal = (items: CartItemType[]) => items.reduce((ack: number, item) => ack + item.amount * item.price, 0);
+	// const calculateTotal = (items: CartItemType[]) => items.reduce((ack: number, item) => ack + item.amount * item.price, 0);
+	
+	const calculateTotal = (items: CartItemType[]) => {
+		return (
+			<h2>
+				Total: ${items.reduce((ack: number, item) => ack + item.amount * item.price, 0)}
+			</h2>
+		);
+	};
 	
 	
 	return (
@@ -39,7 +47,7 @@ const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
 					/>
 				);
 			})}
-			<h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
+			{calculateTotal(cartItems)}
 		</Wrapper>
 	);
 };
