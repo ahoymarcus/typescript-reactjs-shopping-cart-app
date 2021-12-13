c61162bebc8978480168c5d12fb1ec3ac7c0dcf0
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-12 21:20:37 -0300

    Inserir estilo em App.tsx com styled-components. Criar componente de botao para o Carrinho de Compra com o material-ui. Definir o Jsx para os 02 componentes, a secao Carrinho de Compra e o botao para mostrar o carrinho

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,19 @@ import LinearProgress from '@material-ui/core/LinearProgress';
 import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
-
+import IconButton from '@material-ui/core/IconButton';
 
 
 // Styles
 const Wrapper = styled.div`
+	margin: 40px;
+`;
 
+const StyledButton = styled(IconButton)`
+	position: fixed;
+	z-index: 100;
+	right: 20px;
+	top: 20px;
 `;
 
 
@@ -36,12 +43,16 @@ const getProducts = async(): Promise<CartItemType[]> => {
 	return await (await fetch('https://fakestoreapi.com/products')).json();
 };
  
+ 
+ 
 const App = () => {
+	const [cartOpen, setCartOpen ] = useState(false);
+	const [ cartItems, setCartItems ] = useState([] as CartItemType[]);
   const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
 	
 	console.log(data); 
 	
-	const getTotalItems = () => null;
+	const getTotalItems = (items: CartItemType[]) => null;
 	
 	const handleAddToCart = (clickedItem: CartItemType) => null;
 	
@@ -51,18 +62,43 @@ const App = () => {
 	if (error) return <div>Something went wrong...</div>;
 	
 	
-	return (
-    <div className="App">
-      <Grid container spacing={3}>
+	return ( 
+    <Wrapper>
+			<Drawer 
+				anchor='right' 
+				open={cartOpen} 
+				onClose={() => setCartOpen(false)} 
+			>
+				Cart goes here
+			</Drawer>
+			
+			<StyledButton onClick={() => setCartOpen(true)} >
+				<Badge badgeContent={getTotalItems(cartItems)} color='error'>
+					<AddShoppingCartIcon />
+				</Badge>
+			</StyledButton>
+      
+			<Grid 
+				container 
+				spacing={3}
+			>
 				{data?.map((item) => {
 					return (
-						<Grid item key={item.id} xs={12} sm={4}>
-							<Item item={item} handleAddToCart={handleAddToCart} />
+						<Grid 
+							item 
+							key={item.id} 
+							xs={12} 
+							sm={4}
+						>
+							<Item 
+								item={item} 
+								handleAddToCart={handleAddToCart}
+							/>
 						</Grid>
 					);
 				})}
 			</Grid>
-    </div>
+    </Wrapper>
   );
 }
 
